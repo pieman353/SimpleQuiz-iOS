e9476e41411e296d114ff5a667e7132a4a8ab726
Adam Piekarski <adpiekar@lcs-209-131.syr.edu>
2019-02-12 17:06:20 -0500

    Initial Commit

---
+++ assignment2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  assignment2
+//
+//  Created by Adam Piekarski on 2/12/19.
+//  Copyright © 2019 Adam Piekarski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
